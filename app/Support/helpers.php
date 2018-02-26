f74527e05e63ea3dc302fbddedb774a439d897da
Luu Hoang Nam <hoangnam0705@icloud.com>
2018-02-26 10:00:36 +0700

    add app_carbon helper

--- app/Support/helpers.php
+++ app/Support/helpers.php
@@ -1,2 +1,18 @@
 <?php
 
+use Illuminate\Support\Carbon;
+
+if ( ! function_exists('app_carbon')) {
+    /**
+     * Make Carbon instance and set timezone for it based on current app.timezone config
+     *
+     * @param string|null               $time
+     * @param \DateTimeZone|string|null $tz
+     *
+     * @return Carbon
+     */
+    function app_carbon($time = null, $tz = null)
+    {
+        return (new Carbon($time, $tz))->timezone(config('app.timezone'));
+    }
+}
