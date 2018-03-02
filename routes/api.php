09b9afc14c9976587428461c54e792490a19c3c9
Luu Hoang Nam <hoangnam0705@icloud.com>
2018-03-02 22:59:13 +0700

    change post to get for supporting routes

--- routes/api.php
+++ routes/api.php
@@ -17,9 +17,9 @@ Route::middleware('auth:api')->group(function () {
         Route::post('add_item', 'Account\ItemsController@addItem');
 
         // Supports
-        Route::post('suggest_category', 'Account\ItemsController@suggestCategory');
-        Route::post('seller_profiles', 'Account\ItemsController@sellerProfiles');
-        Route::post('allowed_conditions/{category_id}', 'Account\ItemsController@allowedConditions');
+        Route::get('suggest_category', 'Account\ItemsController@suggestCategory');
+        Route::get('seller_profiles', 'Account\ItemsController@sellerProfiles');
+        Route::get('allowed_conditions/{category_id}', 'Account\ItemsController@allowedConditions');
     });
 # END EBAY INTERACTIONS
 });
