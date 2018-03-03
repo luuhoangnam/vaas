56e2d79a2234b962ddf1b9d01df67164fb4bc5d9
Luu Hoang Nam <hoangnam0705@icloud.com>
2018-03-03 20:22:02 +0700

    get previous rank

--- app/Ranking/Record.php
+++ app/Ranking/Record.php
@@ -7,4 +7,29 @@ use Illuminate\Database\Eloquent\Model;
 class Record extends Model
 {
     protected $fillable = ['rank', 'total'];
+
+    public function tracker()
+    {
+        return $this->belongsTo(Tracker::class);
+    }
+
+    public function getPreviousAttribute()
+    {
+        return static::query()
+                     ->where('tracker_id', $this['tracker_id'])
+                     ->where('id', '!=', $this['id'])
+                     ->latest()
+                     ->first();
+    }
+
+    public function getChangeAttribute()
+    {
+        $previous = $this->getPreviousAttribute();
+
+        if (is_null($previous) || is_null($previous['rank']) || is_null($this['rank'])) {
+            return null;
+        }
+
+        return $previous['rank'] - $this['rank'];
+    }
 }
