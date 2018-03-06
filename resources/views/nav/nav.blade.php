6edf4a2b90c36c60c2edce221da6cfef26fcf68f
Luu Hoang Nam <hoangnam0705@icloud.com>
2018-03-06 11:51:54 +0700

    return to home when click to the brand logo

--- resources/views/nav/nav.blade.php
+++ resources/views/nav/nav.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
     <div class="container-fluid">
-        <a class="navbar-brand" href="{{ url('/') }}">
+        <a class="navbar-brand" href="{{ route('home') }}">
             {{ config('app.name', 'Laravel') }}
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
