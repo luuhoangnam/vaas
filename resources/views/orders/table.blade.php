4144c2750092e60819ce4f36a9c3237ff93b9ba6
Luu Hoang Nam <hoangnam0705@icloud.com>
2018-03-06 07:45:52 +0700

    refactoring

---
+++ resources/views/orders/table.blade.php
@@ -0,0 +1,54 @@
+<table class="table table-hover">
+    <thead>
+    <tr>
+        <th scope="col"></th>
+        <th scope="col">Record <i class="fa fa-caret-down"></i></th>
+        <th scope="col">Item</th>
+        <th scope="col">Buyer</th>
+        <th scope="col">Total</th>
+        <th scope="col">FVF</th>
+        <th scope="col">PPF</th>
+        <th scope="col">COG</th>
+        <th scope="col">Profit</th>
+        <th scope="col"></th>
+    </tr>
+    </thead>
+
+    <tbody>
+    @foreach($orders as $order)
+        @php
+            $transaction = $order['transactions']->first();
+            $item = $transaction['item'];
+        @endphp
+
+        <tr>
+            <td scope="row" class="text-right">{{ $order['created_time']->diffForHumans() }}</td>
+            <td><a href="{{ $order['ebay_link'] }}">{{ $order['record'] }}</a></td>
+            <td>
+                (<span class="text-decoration">{{ $transaction['item_id'] }}</span>)
+                <a href="{{ $item['ebay_link'] }}">{{ $transaction['item_title'] }}</a>
+            </td>
+            <td><a href="{{ $order['buyer_ebay_link'] }}">{{ $order['buyer_username'] }}</a></td>
+            <td>{{ usd($order['total']) }}</td>
+            <td>{{ usd($order['final_value_fee']) }}</td>
+            <td>{{ usd($order['paypal_fee']) }}</td>
+            <td>{{ $order['cog'] ? usd($order['cog']) : 'N/A' }}</td>
+            <td>
+                <span class="{{ $order['profit'] > 0 ? 'text-success' : 'text-warning' }}">
+                    {{ $order['profit'] ? usd($order['profit']) : 'N/A' }}
+                </span>
+            </td>
+            <td class="text-right">
+                @if ($item['cashback_link'])
+                    <a class="btn btn-primary btn-sm" target="_blank"
+                       href="{{ $item['cashback_link'] }}">
+                        Place Order
+                    </a>
+                @else
+                    <span class="text-muted">Can Not Fetch</span>
+                @endif
+            </td>
+        </tr>
+    @endforeach
+    </tbody>
+</table>
