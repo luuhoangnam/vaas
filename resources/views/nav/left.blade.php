3c277e9d588f73e802317773f9fc86da8c9b3c04
Luu Hoang Nam <hoangnam0705@icloud.com>
2018-03-06 10:54:33 +0700

    add left menu item

--- resources/views/nav/left.blade.php
+++ resources/views/nav/left.blade.php
@@ -4,4 +4,5 @@
 <li><a class="nav-link {{ request()->is('orders*') ? 'active' : '' }}" href="{{ route('orders') }}">Orders</a></li>
 <li><a class="nav-link {{ request()->is('feedbacks*') ? 'active' : '' }}" href="#">Feedbacks</a></li>
 <li><a class="nav-link {{ request()->is('returns*') ? 'active' : '' }}" href="#">Returns</a></li>
+<li><a class="nav-link {{ request()->is('automation*') ? 'active' : '' }}" href="#">Automation</a></li>
 @endbeta
