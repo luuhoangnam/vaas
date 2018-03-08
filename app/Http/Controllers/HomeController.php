f15ef1c4b45c5787ef350496c8d9d8b41d5203e2
Luu Hoang Nam <hoangnam0705@icloud.com>
2018-03-08 13:19:01 +0700

    prepare for dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,8 +6,9 @@ use Illuminate\Http\Request;
 
 class HomeController extends AuthRequiredController
 {
-    public function index()
+    public function index(Request $request)
     {
+        // Dashboard Rendering
         return view('home');
     }
 }
