e63914aebe65c1ca6f4ebf0e92406944c8db5c81
Luu Hoang Nam <hoangnam0705@icloud.com>
2018-03-22 16:52:59 +0700

    bak

---
+++ app/Support/CategoryResolver.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Support;
+
+class CategoryResolver
+{
+    public function parentOf($categoryID)
+    {
+
+    }
+
+    public function allParentsOf($categoryID)
+    {
+
+    }
+
+    public function rootOf($categoryID)
+    {
+
+    }
+
+    public function children($categoryID)
+    {
+
+    }
+
+    public function isParentOf()
+    {
+
+    }
+
+    public function isChildOf()
+    {
+
+    }
+}
