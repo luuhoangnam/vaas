348d9e4b253d1ab200e1c39291ed782c19c65c66
Luu Hoang Nam <hoangnam0705@icloud.com>
2018-03-24 15:39:22 +0700

    add bulk of data

--- app/Http/Controllers/API/ResearchItemController.php
+++ app/Http/Controllers/API/ResearchItemController.php
@@ -9,10 +9,18 @@ use App\Http\Controllers\Controller;
 use App\Item;
 use App\Sourcing\AmazonAPI;
 use App\Sourcing\AmazonCrawler;
+use Carbon\Carbon as PureCarbon;
 use DTS\eBaySDK\Trading\Enums\AckCodeType;
+use DTS\eBaySDK\Trading\Enums\DetailLevelCodeType;
 use DTS\eBaySDK\Trading\Types\GetItemRequestType;
+use DTS\eBaySDK\Trading\Types\GetItemTransactionsRequestType;
+use DTS\eBaySDK\Trading\Types\GetItemTransactionsResponseType;
 use DTS\eBaySDK\Trading\Types\ItemType;
+use DTS\eBaySDK\Trading\Types\PaginationType;
+use DTS\eBaySDK\Trading\Types\TransactionType;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Collection;
 
 class ResearchItemController extends Controller
 {
@@ -26,17 +34,24 @@ class ResearchItemController extends Controller
         $request->ItemID = (string)$id;
 
         /** @noinspection PhpMethodParametersCountMismatchInspection */
-        $response = $this->trading()->getItem($request, 15);
+        $response = $this->trading()->getItem($request, 60); // Cached for 1 Hour
 
         if ($response->Ack === AckCodeType::C_FAILURE) {
             throw new TradingApiException($request, $response);
         }
 
-        $item = $response->Item;
+        $transactions = $this->performance($id);
 
+        return array_merge($this->extract($response->Item), compact('transactions'));
+    }
+
+    public function extract(ItemType $item): array
+    {
         return [
             'item_id'              => $item->ItemID,
             'title'                => $item->Title,
+            'site'                 => $item->Site,
+            'condition'            => $item->ConditionDisplayName,
             'primary_category'     => [
                 'id'      => (int)$item->PrimaryCategory->CategoryID,
                 'name'    => $item->PrimaryCategory->CategoryName,
@@ -55,14 +70,59 @@ class ResearchItemController extends Controller
             'start_time'           => app_carbon($item->ListingDetails->StartTime)->toDateTimeString(),
             'end_time'             => app_carbon($item->ListingDetails->EndTime)->toDateTimeString(),
             'listing_type'         => $item->ListingType,
-            'condition'            => $item->ConditionDisplayName,
             'has_variants'         => (bool)$item->Variations,
             'is_top_rated_listing' => $item->TopRatedListing,
-            'watch_count'          => $item->WatchCount,
             'attributes'           => $this->normalizeAttribute($item),
             'source'               => $this->guessSource($item),
             'listed_on'            => $item->SKU ? $this->listedOn($item->SKU) : null,
         ];
+    }
+
+    protected function performance($itemID): array
+    {
+        $request = new GetItemTransactionsRequestType;
+
+        $request->ItemID       = (string)$itemID;
+        $request->Pagination   = new PaginationType(['EntriesPerPage' => 100, 'PageNumber' => 1]);
+        $request->NumberOfDays = 30;
+
+        $request->DetailLevel = [DetailLevelCodeType::C_RETURN_ALL];
+
+        /** @noinspection PhpMethodParametersCountMismatchInspection */
+        $response = $this->trading()->getItemTransactions($request, 60 * 24); // Cached for 1 Day
+//        dd($response->toArray());
+        try {
+            $transactions = $this->transactions($response);
+        } catch (\Exception $exception) {
+            throw $exception;
+        }
+
+        // Transaction Last 30 Days
+        $now = Carbon::now();
+
+        $sold7d  = $this->countTransactionForPeriod($transactions, (clone $now)->subDays(7), $now);
+        $sold14d = $this->countTransactionForPeriod($transactions, (clone $now)->subDays(14), $now);
+        $sold21d = $this->countTransactionForPeriod($transactions, (clone $now)->subDays(21), $now);
+        $sold30d = $this->countTransactionForPeriod($transactions, (clone $now)->subDays(30), $now);
+
+        return [
+            'average_price'    => $this->averagePrice($transactions),
+            'average_quantity' => $this->averageQuantity($transactions),
+            'sold_7d'          => $sold7d,
+            'sold_14d'         => $sold14d,
+            'sold_21d'         => $sold21d,
+            'sold_30d'         => $sold30d,
+            'chart'            => $this->transactionChart($transactions),
+        ];
+    }
+
+    protected function countTransactionForPeriod(Collection $transactions, PureCarbon $since, PureCarbon $until): int
+    {
+        $filtered = $transactions->filter(function (TransactionType $transaction) use ($since, $until) {
+            return app_carbon($transaction->CreatedDate)->between($since, $until);
+        });
+
+        return $filtered->count();
     }
 
     protected function normalizeAttribute(ItemType $item)
@@ -104,7 +164,7 @@ class ResearchItemController extends Controller
             return AmazonCrawler::get($item->SKU);
         });
     }
-    
+
     protected function listedOn($sku)
     {
         return Item::query()
@@ -113,5 +173,138 @@ class ResearchItemController extends Controller
                    ->get()
                    ->pluck('account.username')
                    ->values();
+    }
+
+    protected function transactions(GetItemTransactionsResponseType $response): Collection
+    {
+        if (is_null(@$response->TransactionArray->Transaction)) {
+            return new Collection;
+        }
+
+        return new Collection($response->TransactionArray->Transaction);
+    }
+
+    protected function averagePrice(Collection $transactions)
+    {
+        return $transactions->average(function (TransactionType $transaction) {
+            return $transaction->TransactionPrice->value;
+        });
+    }
+
+    protected function averageQuantity(Collection $transactions)
+    {
+        return $transactions->average(function (TransactionType $transaction) {
+            return $transaction->QuantityPurchased;
+        });
+    }
+
+    protected function transactionChart(Collection $transactions)
+    {
+        $minDate = $transactions->min(function (TransactionType $transactionType) {
+            return $transactionType->CreatedDate;
+        });
+
+        $maxDate = $transactions->max(function (TransactionType $transactionType) {
+            return $transactionType->CreatedDate;
+        });
+
+        $minDate = app_carbon($minDate);
+        $maxDate = app_carbon($maxDate);
+
+        $dates = date_range($minDate, $maxDate);
+
+        $data = $dates->map(function (PureCarbon $date) use ($transactions) {
+            $filtered = $transactions->filter(function (TransactionType $transaction) use ($date) {
+                return $date->isSameDay(
+                    app_carbon($transaction->CreatedDate)
+                );
+            })->map(function (TransactionType $transaction) {
+                return $transaction->TransactionPrice->value;
+            });
+
+            return [
+                'date'    => $date->toDateString(),
+                'count'   => $filtered->count(),
+                'revenue' => round($filtered->sum(), 2),
+            ];
+        });
+
+        $orders   = $data->pluck('count');
+        $revenues = $data->pluck('revenue');
+
+        $orderAxisStep = round($orders->max());
+        $maxOrdersAxis = (round($orders->max() / $orderAxisStep) + 1) * $orderAxisStep;
+
+        $revenueAxisStep = round($revenues->max());
+        $maxRevenueAxis  = (round($revenues->max() / $revenueAxisStep) + 1) * $revenueAxisStep;
+
+        return [
+            'type' => 'bar',
+
+            'data' => [
+                'labels'   => $dates->toDateStringCollection()->toArray(),
+                'datasets' => [
+                    [
+                        'type'            => 'bar',
+                        'label'           => 'Orders',
+                        'backgroundColor' => "rgba(54, 162, 235, 0.2)",
+                        'borderColor'     => "rgb(54, 162, 235)",
+                        'data'            => $orders->toArray(),
+                        'yAxisID'         => 'orders',
+                    ],
+                    [
+                        'type'            => 'line',
+                        'label'           => 'Revenue',
+                        'fill'            => false,
+                        'backgroundColor' => "rgba(255, 205, 86, 0.2)",
+                        'borderColor'     => "rgb(255, 205, 86)",
+                        'data'            => $revenues->toArray(),
+                        'yAxisID'         => 'revenue',
+                    ],
+                ],
+            ],
+
+            'options' => [
+                'tooltips' => [
+                    'titleFontFamily' => 'Fira Code',
+                ],
+                'scales'   => [
+                    'xAxes' => [
+                        [
+                            'gridLines' => [
+                                'display' => false,
+                            ],
+                        ],
+                    ],
+                    'yAxes' => [
+                        [
+                            'id'        => 'orders',
+                            'position'  => 'left',
+                            'ticks'     => [
+                                'beginAtZero' => true,
+                                'max'         => $maxOrdersAxis,
+                                'stepSize'    => $orderAxisStep,
+                            ],
+                            'gridLines' => [
+                                'display' => false,
+                            ],
+                        ],
+                        [
+                            'id'        => 'revenue',
+                            'position'  => 'right',
+                            'ticks'     => [
+                                'beginAtZero' => true,
+                                'max'         => $maxRevenueAxis,
+                                'stepSize'    => $revenueAxisStep,
+                            ],
+                            'gridLines' => [
+                                'display' => false,
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ];
+
     }
 }
