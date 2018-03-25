c92c426b35b8c2ab7cc394a2e724ad17cb75d23a
Luu Hoang Nam <hoangnam0705@icloud.com>
2018-03-25 12:36:49 +0700

    add firebase

--- app/Console/Commands/UpdateeBayAPIQuota.php
+++ app/Console/Commands/UpdateeBayAPIQuota.php
@@ -8,12 +8,14 @@ use DTS\eBaySDK\Trading\Enums\AckCodeType;
 use DTS\eBaySDK\Trading\Enums\DetailLevelCodeType;
 use DTS\eBaySDK\Trading\Types\GetApiAccessRulesRequestType;
 use Illuminate\Console\Command;
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\ServiceAccount;
 
 class UpdateeBayAPIQuota extends Command
 {
     protected $signature = 'api:quota';
 
-    protected $description = 'Update API Quota in Cache';
+    protected $description = 'Update API Quota in Cache and Firebase';
 
     public function handle()
     {
@@ -23,6 +25,8 @@ class UpdateeBayAPIQuota extends Command
 
         cache()->put('X-API-LIMIT-USAGE', $usage, $cacheTime);
         cache()->put('X-API-LIMIT-QUOTA', $hardLimit, $cacheTime);
+
+        $this->updateOnFirebase($usage, $hardLimit);
     }
 
     protected function usage()
@@ -45,5 +49,20 @@ class UpdateeBayAPIQuota extends Command
         }
 
         return null;
+    }
+
+    protected function updateOnFirebase($usage, $quota)
+    {
+        $serviceAccount = ServiceAccount::fromJsonFile(resource_path('credentials/vaas-quick-863f4a7a64e2.json'));
+
+        $firebase = (new Factory)
+            ->withServiceAccount($serviceAccount)
+            // The following line is optional if the project id in your credentials file
+            // is identical to the subdomain of your Firebase project. If you need it,
+            // make sure to replace the URL with the URL of your project.
+            ->withDatabaseUri('https://vaas-quick.firebaseio.com/')
+            ->create();
+
+        $firebase->getDatabase()->getReference('api_limit')->update(compact('usage', 'quota'));
     }
 }
