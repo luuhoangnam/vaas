3748d25b2db3e03d4143333bfc3c4aba54ae1828
Luu Hoang Nam <hoangnam0705@icloud.com>
2018-03-25 13:59:24 +0700

    default for all

--- config/database.php
+++ config/database.php
@@ -115,20 +115,6 @@ return [
             'database' => 0,
         ],
 
-        'queue' => [
-            'host'     => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port'     => env('REDIS_PORT', 6379),
-            'database' => 1,
-        ],
-
-        'cache' => [
-            'host'     => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port'     => env('REDIS_PORT', 6379),
-            'database' => 2,
-        ],
-
     ],
 
 ];
