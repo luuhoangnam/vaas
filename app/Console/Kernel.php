dedca2083c57f3fb37f5fcd264b6dc125f493287
Luu Hoang Nam <hoangnam0705@icloud.com>
2018-03-26 07:04:33 +0700

    make it work

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -60,7 +60,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('api:quota')->everyMinute();
+        $schedule->command('api:quota')->everyFiveMinutes();
         $schedule->command('horizon:snapshot')->everyFiveMinutes();
         $schedule->command("ebay:sync:all --only_orders --since '3 hours ago'")->hourly(); // Fix
         $schedule->command('ranking:refresh')->weekly();
