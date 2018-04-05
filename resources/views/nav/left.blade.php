ec4c2f0a0ee20a7bdfe984402b11581d78e3ff2b
Luu Hoang Nam <hoangnam0705@icloud.com>
2018-04-05 09:16:34 +0700

    clean up

--- resources/views/nav/left.blade.php
+++ resources/views/nav/left.blade.php
@@ -2,8 +2,5 @@
     @beta
     <li><a class="nav-link {{ active_on('items*') }}" href="{{ route('items') }}">Listings</a></li>
     <li><a class="nav-link {{ active_on('orders*') }}" href="{{ route('orders') }}">Orders</a></li>
-    <li><a class="nav-link {{ active_on('feedbacks*') }}" href="#">Feedbacks</a></li>
-    <li><a class="nav-link {{ active_on('returns*') }}" href="#">Returns</a></li>
-    <li><a class="nav-link {{ active_on('automation*') }}" href="#">Automation</a></li>
     @endbeta
 @endauth
