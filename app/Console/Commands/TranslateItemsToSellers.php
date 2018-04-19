50303de0e24471fa69e6713621224da5ed98e0a8
Luu Hoang Nam <hoangnam0705@icloud.com>
2018-04-19 14:58:38 +0700

    Change non seller tag for easier track

--- app/Console/Commands/TranslateItemsToSellers.php
+++ app/Console/Commands/TranslateItemsToSellers.php
@@ -55,7 +55,7 @@ class TranslateItemsToSellers extends Command
 
                 return $response->Item->Seller->UserID;
             } catch (Exception $exception) {
-                return '<Invalid Seller>';
+                return '[Invalid Seller]';
             }
         })->unique();
 
