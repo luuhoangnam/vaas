a89df5e4d49f1777f53a69c5e2a7db69407c47a5
Luu Hoang Nam <hoangnam0705@icloud.com>
2018-05-13 14:47:54 +0700

    search seller

--- resources/views/nav/left.blade.php
+++ resources/views/nav/left.blade.php
@@ -2,5 +2,6 @@
     @beta
     <li><a class="nav-link {{ active_on('items*') }}" href="{{ route('items') }}">Listings</a></li>
     <li><a class="nav-link {{ active_on('orders*') }}" href="{{ route('orders') }}">Orders</a></li>
+    <li><a class="nav-link {{ active_on('search/seller*') }}" href="{{ route('search.seller') }}">Search Sellers</a></li>
     @endbeta
 @endauth
