8dff47a3f6d5a8b13307f8b27425f75aecc6e1ff
Luu Hoang Nam <hoangnam0705@icloud.com>
2018-05-14 17:18:16 +0700

    firebase

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -20,7 +20,21 @@ try {
     require('bootstrap');
 
     // Setup Bootstrap JS
-    $(() => $('[data-toggle="tooltip"]').tooltip())
+    $(() => $('[data-toggle="tooltip"]').tooltip());
+
+    // Firebase
+    const config = {
+        apiKey: "AIzaSyCh2JzXMnYajBzt7ytjHVe_Q6sXNFLb6YE",
+        authDomain: "merced-13.firebaseapp.com",
+        databaseURL: "https://merced-13.firebaseio.com",
+        projectId: "merced-13",
+        storageBucket: "merced-13.appspot.com",
+        messagingSenderId: "1094482201047"
+    };
+
+    firebase.initializeApp(config);
+
+    window.firebase = firebase;
 } catch (e) {
 }
 
